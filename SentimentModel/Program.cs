74ac24109a34e68d82fa93396d85bf07e186f2e1
Justus Storm <storm.j.dev@gmail.com>
2022-01-25 23:26:13 -0600

    Inital commit

---
+++ SentimentModel/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SentimentModel
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
