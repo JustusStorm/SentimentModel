e9e38c55c110cac09748a3587c32f9874635de67
Justus Storm <storm.j.dev@gmail.com>
2022-01-26 00:03:21 -0600

    Finished program

--- SentimentModel/SentimentModel/Program.cs
+++ SentimentModel/SentimentModel/Program.cs
@@ -6,7 +6,24 @@ namespace SentimentModel
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Type a positive or negative comment that you would say about some resturant, business, etc..");
+            Console.Write("> ");
+            var userInput = Console.ReadLine();
+
+
+            // Add input data
+            var sampleData = new SentimentModel.ModelInput()
+            {
+                Col0 = userInput
+            };
+
+            // Load model and predict output of sample data
+            var result = SentimentModel.Predict(sampleData);
+
+            // If Prediction is 1, sentiment is "Positive"; otherwise, sentiment is "Negative"
+            string sentiment = result.Prediction == 1 ? "Positive" : "Negative";
+            Console.WriteLine($"Text: {sampleData.Col0}\nSentiment: {sentiment}");
+
         }
     }
 }
